b28324d6745624d98c8fee510f715519cdf89d0f
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-15 23:11:29 +0300

    Writing the basics of the main app.py file

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from server.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
